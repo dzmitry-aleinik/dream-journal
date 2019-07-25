64137c2748feba62372577a2ae829d9e96496cd6
dzmitry-aleinik-itechart <dzmitry.aleinik@itechart-group.com>
2019-07-25 15:15:08 +0300

    Delete custom migrations

--- db/migrate/20190521080645_create_join_table_users_roles.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUsersRoles < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :users, :roles, table_name: :users_roles   do |t|
-      t.index :user_id
-      t.index :role_id
-    end
-  end
-end
--- db/migrate/20190521125953_add_name_to_roles.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToRoles < ActiveRecord::Migration[5.2]
-  def change
-    add_column :roles, :name, :string
-  end
-end
